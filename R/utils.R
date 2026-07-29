acca746613deca2a42186dcd6c07cf378c234dd9
ielbadisy <elbadisyimad@gmail.com>
2026-07-29 12:32:06 +0100

    Update utils for survival-only helpers

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,4 @@
-fastgbm_sigmoid <- function(x) {
-  1 / (1 + exp(-pmin(pmax(x, -35), 35)))
-}
-
-fastgbm_as_matrix <- function(x) {
+survgbm_as_matrix <- function(x) {
   if (is.data.frame(x)) {
     return(model.matrix(~ . - 1, data = x))
   }
@@ -13,10 +9,21 @@ fastgbm_as_matrix <- function(x) {
   stop("Unsupported input type for `x`.", call. = FALSE)
 }
 
-fastgbm_model_fields <- function(object) {
+survgbm_model_fields <- function(object) {
   c("objective", "ntrees", "learning_rate", "max_depth", "min_node_size")
 }
 
 `%||%` <- function(x, y) {
   if (is.null(x)) y else x
+}
+
+# Internal-only: exposes the compiled gradient/Hessian kernels for testing.
+survgbm_grad_hess <- function(pred, time, status, objective) {
+  .Call(
+    "survgbm_grad_hess_cpp",
+    as.numeric(pred),
+    as.numeric(time),
+    as.integer(status),
+    as.character(objective)
+  )
 }
