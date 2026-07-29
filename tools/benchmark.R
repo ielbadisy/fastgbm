4a64bedf15e0c89f06cc2279b774ec2859cf0167
ielbadisy <elbadisyimad@gmail.com>
2026-07-29 12:32:16 +0100

    Update benchmark.R tool for survival-only API

--- tools/benchmark.R
+++ tools/benchmark.R
@@ -1,7 +1,10 @@
-library(fastgbm)
+library(survgbm)
 
 set.seed(1)
-x <- matrix(rnorm(1000), ncol = 5)
-y <- x[, 1] * 0.5 - x[, 2] * 0.25 + rnorm(nrow(x), sd = 0.1)
-fit <- fastgbm(x, y, objective = "reg:squarederror", ntrees = 20L, verbose = FALSE)
+n <- 1000
+x <- matrix(rnorm(n * 5), ncol = 5)
+lp_true <- x[, 1] * 0.5 - x[, 2] * 0.25
+time <- rexp(n, rate = exp(lp_true) * 0.2)
+status <- rbinom(n, 1, 0.8)
+fit <- survgbm(x, time = time, status = status, objective = "cox", ntrees = 20L, verbose = FALSE)
 print(summary(fit))
