e4450fefcd2f4fdaf1886c5b865f543ea336cf0a
ielbadisy <elbadisyimad@gmail.com>
2026-07-29 12:32:06 +0100

    Update serialization for survival-only fit object fields

--- R/serialization.R
+++ R/serialization.R
@@ -1,8 +1,19 @@
-save_fastgbm <- function(object, path) {
+#' Save a fitted survgbm model
+#'
+#' @param object A fitted `survgbm` object.
+#' @param path Path to write the serialized model to.
+#' @return `path`, invisibly.
+#' @export
+save_survgbm <- function(object, path) {
   saveRDS(object, path)
   invisible(path)
 }
 
-load_fastgbm <- function(path) {
+#' Load a serialized survgbm model
+#'
+#' @param path Path to a serialized model, as written by [save_survgbm()].
+#' @return A `survgbm` object.
+#' @export
+load_survgbm <- function(path) {
   readRDS(path)
 }
