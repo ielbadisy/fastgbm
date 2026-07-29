ded6978d7f6bd54c3198165baf0eb5f918501e7d
ielbadisy <elbadisyimad@gmail.com>
2026-07-29 12:32:06 +0100

    Update init.c registration for survgbm_fit_cpp's extended (21-arg) signature

--- src/init.c
+++ src/init.c
@@ -2,23 +2,27 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
-SEXP fastgbm_fit_cpp(SEXP xSEXP, SEXP ySEXP, SEXP timeSEXP, SEXP statusSEXP,
+SEXP survgbm_fit_cpp(SEXP xSEXP, SEXP timeSEXP, SEXP statusSEXP,
                      SEXP objectiveSEXP, SEXP ntreesSEXP, SEXP learning_rateSEXP,
                      SEXP max_depthSEXP, SEXP min_node_sizeSEXP, SEXP max_binsSEXP,
                      SEXP subsampleSEXP, SEXP colsampleSEXP, SEXP lambdaSEXP,
                      SEXP gammaSEXP, SEXP min_child_weightSEXP, SEXP seedSEXP,
-                     SEXP verboseSEXP);
-SEXP fastgbm_predict_cpp(SEXP modelSEXP, SEXP xSEXP, SEXP typeSEXP);
-SEXP fastgbm_importance_cpp(SEXP modelSEXP);
+                     SEXP threadsSEXP, SEXP verboseSEXP,
+                     SEXP x_validSEXP, SEXP time_validSEXP, SEXP status_validSEXP,
+                     SEXP early_stoppingSEXP);
+SEXP survgbm_predict_cpp(SEXP modelSEXP, SEXP xSEXP, SEXP typeSEXP);
+SEXP survgbm_importance_cpp(SEXP modelSEXP);
+SEXP survgbm_grad_hess_cpp(SEXP predSEXP, SEXP timeSEXP, SEXP statusSEXP, SEXP objectiveSEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"fastgbm_fit_cpp", (DL_FUNC) &fastgbm_fit_cpp, 17},
-    {"fastgbm_predict_cpp", (DL_FUNC) &fastgbm_predict_cpp, 3},
-    {"fastgbm_importance_cpp", (DL_FUNC) &fastgbm_importance_cpp, 1},
+    {"survgbm_fit_cpp", (DL_FUNC) &survgbm_fit_cpp, 21},
+    {"survgbm_predict_cpp", (DL_FUNC) &survgbm_predict_cpp, 3},
+    {"survgbm_importance_cpp", (DL_FUNC) &survgbm_importance_cpp, 1},
+    {"survgbm_grad_hess_cpp", (DL_FUNC) &survgbm_grad_hess_cpp, 4},
     {NULL, NULL, 0}
 };
 
-void R_init_fastgbm(DllInfo *dll) {
+void R_init_survgbm(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
